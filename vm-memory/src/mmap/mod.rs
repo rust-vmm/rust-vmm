rust-vmm/vm-memory

xen: rename MmapRegion to GuestRegionXen
It plays the same role as GuestRegionMmap for unix, so update the name
to provide some consistency between the two.

Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

src/mmap/mod.rs
@@ -28,7 +28,7 @@ pub(super) mod windows;
 pub use unix::{Error as MmapRegionError, MmapRegion, MmapRegionBuilder};
 
 #[cfg(all(feature = "xen", target_family = "unix"))]
-pub use xen::{Error as MmapRegionError, MmapRange, MmapRegion, MmapXenFlags};
+pub use xen::{Error as MmapRegionError, GuestRegionXen, MmapRange, MmapXenFlags};
 
 #[cfg(target_family = "windows")]
 pub use std::io::Error as MmapRegionError;
@@ -112,7 +112,7 @@ pub(crate) mod tests {
         #[cfg(all(unix, feature = "backend-mmap"))]
         Mmap[crate::mmap::unix::GuestRegionMmap<()>],
         #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
-        Xen[crate::mmap::xen::MmapRegion]
+        Xen[crate::mmap::xen::GuestRegionXen]
     }
 
     // The cfgs make using vec![...] instead more unreadable, so suppress the lint here.
@@ -138,7 +138,7 @@ pub(crate) mod tests {
             ));
             #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
             regions.push(AnyRegion::Xen(
-                crate::MmapRegionXen::from_range(crate::MmapRange::new_unix(
+                crate::GuestRegionXen::from_range(crate::MmapRange::new_unix(
                     size,
                     Some(f_off.clone()),
                     addr,
@@ -172,7 +172,7 @@ pub(crate) mod tests {
             ));
             #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
             regions.push(AnyRegion::Xen(
-                crate::MmapRegionXen::from_range(crate::MmapRange::new_unix(size, None, addr))
+                crate::GuestRegionXen::from_range(crate::MmapRange::new_unix(size, None, addr))
                     .unwrap(),
             ));
             regions

rust-vmm/rust-vmm

vm-memory/src/mmap/mod.rs
@@ -32,7 +32,7 @@ pub use unix::{
 #[cfg(all(feature = "xen", target_family = "unix"))]
 pub use xen::{
     Error as MmapRegionErrorXen, GuestMemoryMmap as GuestMemoryMmapXen, GuestMemoryXen,
-    GuestRegionMmap as GuestRegionMmapXen, MmapRangeXen, MmapRegion as MmapRegionXen, MmapXenFlags,
+    GuestRegionMmap as GuestRegionMmapXen, GuestRegionXen, MmapRangeXen, MmapXenFlags,
 };
 
 #[cfg(target_family = "windows")]
@@ -117,7 +117,7 @@ pub(crate) mod tests {
         #[cfg(all(unix, feature = "backend-mmap"))]
         Mmap[crate::GuestRegionMmap<()>],
         #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
-        Xen[crate::MmapRegionXen]
+        Xen[crate::GuestRegionXen]
     }
 
     // The cfgs make using vec![...] instead more unreadable, so suppress the lint here.
@@ -143,7 +143,7 @@ pub(crate) mod tests {
             ));
             #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
             regions.push(AnyRegion::Xen(
-                crate::MmapRegionXen::from_range(crate::MmapRangeXen::new_unix(
+                crate::GuestRegionXen::from_range(crate::MmapRangeXen::new_unix(
                     size,
                     Some(f_off.clone()),
                     addr,
@@ -167,7 +167,7 @@ pub(crate) mod tests {
 
                 #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
                 AnyRegion::Xen(region) => AnyRegion::Xen(
-                    crate::MmapRegionXen::from_range(crate::MmapRangeXen::new_unix(
+                    crate::GuestRegionXen::from_range(crate::MmapRangeXen::new_unix(
                         crate::GuestMemoryRegion::len(region) as usize,
                         None,
                         addr,
@@ -197,7 +197,7 @@ pub(crate) mod tests {
             ));
             #[cfg(all(unix, feature = "backend-mmap", feature = "xen"))]
             regions.push(AnyRegion::Xen(
-                crate::MmapRegionXen::from_range(crate::MmapRangeXen::new_unix(size, None, addr))
+                crate::GuestRegionXen::from_range(crate::MmapRangeXen::new_unix(size, None, addr))
                     .unwrap(),
             ));
             regions